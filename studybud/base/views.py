d03a457a9e72edce712c3a553774b1325e789ddf
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-11-01 15:11:35 -0500

    changed rooms to db data

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Room
 
 rooms = [{'id':1, 'name': 'learn python'}, {'id':2, 'name': 'design'}, {'id':3, 'name': 'frontend'}, ]
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'home.html', {'rooms':rooms})
 
 def room(request, id):
