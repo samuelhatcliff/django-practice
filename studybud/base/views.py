fbe6feb5c947abb6a436254b78ae3aa9e36a1599
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-11-01 19:58:12 -0500

    added update functionality to room form

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -21,7 +21,19 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            print("FORM WAS VALID11")
             return redirect('home')
+    context = {'form':form}
+    return render(request, 'room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form':form}
     return render(request, 'room_form.html', context)
